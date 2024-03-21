75b9696ec410e67a55308a3df0edb43d6c6b2704
Jinseo Kim <130826295+kralj627@users.noreply.github.com>
2024-03-21 16:28:52 +0900

    Create Calculator.cs

---
+++ Calculator.cs
@@ -0,0 +1,12 @@
+public class Calculator
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+}
